9b244027a25f7219e5e5e4f281fecb07b71a8c31
Tinus Nel <tinus.home@gmail.com>
2012-09-13 21:27:51 +0200

    Added rule creation

--- src/org/flying/lions/RuleReader.java
+++ src/org/flying/lions/RuleReader.java
@@ -1,7 +1,9 @@
 package org.flying.lions;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,6 +11,7 @@ import java.util.Scanner;
 import java.io.File;
 
 import android.os.Environment;
+import android.util.Log;
 
 
 public class RuleReader {
@@ -26,19 +29,44 @@ public class RuleReader {
 
     public void iniRules() throws IOException {
      
+    	String tempString = null;
         File sdcard = Environment.getExternalStorageDirectory();
 
       //Get the text file
-        File file = new File(sdcard + "/MEM/ORI",confFileName);
-    	
-        Scanner sc = new Scanner(file);
+        File folder = new File(sdcard + "/MEM/ORI");
+        folder.mkdirs();
         
-        String tempString = sc.nextLine();
+       	File file = new File(sdcard + "/MEM/ORI",confFileName);   
         
-        
-        sc.close();
+       	if(file.exists())
+        {
+            Scanner sc = new Scanner(file);
+            
+            tempString = sc.nextLine();
+
+            sc.close();
+
+        }
+        	
+        else
+        {
+	   		 FileWriter fileWriter = new FileWriter("/mnt/sdcard/MEM/ORI/" + confFileName);
+	   		 
+	   		 if(confFileName.equals("ABSARules.ini"))
+	   			 tempString = "<Absa,;> <accName,,> <TransactionType,,> <Date,;> <Location,,> <\"R\"amount,,> * <\"R\"balance,.> * * *";
+	   			 	   		 
+	   		 if(confFileName.equals("FNBRules.ini"))
+	   			 tempString = "<FNB,;> * <\"R\"Cost,;> * * <TransactionType,;> <location,\"@\"|\"from\"|\".\"> * <accName,\"from\"> * * * <\"R\"balance,.> * * <Time,;*>";
+	   			 
+	   		 fileWriter.write(tempString);
+	
+		   	 fileWriter.flush();
+		   	 fileWriter.close();
+        	
+        }
         
         tokenedRules = tempString.split(" ");
+       
     }
 
 
